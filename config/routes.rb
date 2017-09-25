bd9fdfdd54d8a0307f4dd95f83a89d1bcd4f2b69
森田啓太 <mancheunited7>
2017-09-25 16:20:51 +0900

    heroku3

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
 
   root "top#index"
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
